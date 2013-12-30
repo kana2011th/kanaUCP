81e8e2c7df33bfb834d97d6f5902a83529560e88
kana2011th <khwd.2007@gmail.com>
2013-12-30 22:28:05 +0700

    1st commit

---
+++ inc/general.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author kana2011th
+ */
+class cmsGeneral {
+
+    private $general;
+
+    public function isAjaxReq() {
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//config
+$config["d"]["header"] = "header";
+$config["d"]["footer"] = "footer";
+$config["d"]["svname"] = "MC:RE";
+$config["d"]["header"] = "header";
+
+
+
+
+
+
+
+
+
+
+
+
+
+include 'inc/general.php';
+$cms = new cmsGeneral();
+?>
