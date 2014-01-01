6e6af13946ed46eb3e933f64a7594dba0ef95527
kana2011th <khwd.2007@gmail.com>
2014-01-01 12:16:22 +0700

    move files and make something (edit)

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// Database Settings, Match this to your server.
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_DATABASE', 'authme');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'kana2011');
+
+
+
+// Server Setting
+define('SV_NAME', 'MC:RE');
+define('SV_IP', '127.0.0.1');
+define('SV_HOSTNAME', 'local.rockerger.com');
