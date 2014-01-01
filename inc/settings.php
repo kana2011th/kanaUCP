3c5974c72ecaceafa2256cebffeed8f367e7b483
kana2011th <khwd.2007@gmail.com>
2014-01-01 12:11:17 +0700

    move files and make something (edit)

---
+++ inc/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// Database Settings, Match this to your server.
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_DATABASE', 'minecraft');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
