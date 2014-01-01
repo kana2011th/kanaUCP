0736764c4cbfc131540762c904dd0b14c9625ab6
x7d <x7dtv@hotmail.com>
2014-01-01 10:21:56 +0700

    Update include.inc.php
    Added setting config

--- inc/include.inc.php
+++ inc/include.inc.php
@@ -6,6 +6,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+require 'settings.php';
+ 
 include 'general.class.php';
 include 'database.class.php';
 include 'account.class.php';
